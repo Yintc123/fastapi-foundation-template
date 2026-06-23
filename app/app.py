5d6b92504dcb956319bb4e024ba720fc5af77698
尹騰慶 <yiqazwsx123@gmail.com>
2026-06-23 09:36:36 +0800

    Add API, health().

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from .api import api_router
 
 def create_app() -> FastAPI:
     app: FastAPI = FastAPI(
         title = "fastapi-foundation-template",
         version = "1.0.0"
     )
+
+    app.include_router(api_router)
 
     return app
